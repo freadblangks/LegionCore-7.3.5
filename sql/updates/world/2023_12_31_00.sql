8dd0a9f5cf9c121352b1ed84ade19e35e4469df5
SoulSeekkor <soulseekkor@live.com>
2023-12-31 16:36:16 -0600

    Another quest chance fix.

--- sql/updates/world/2023_12_31_00.sql
+++ sql/updates/world/2023_12_31_00.sql
@@ -11,6 +11,7 @@
 -- 3876 = Fang of Bhag'thera
 -- 3877 = Talon of Tethis
 -- 3879 = Paw of Sin'Dall
+-- 3880 = Head of Bangalash
 -- 4106 = Tumbled Crystal
 -- 22934 = Lasher Sample
 -- 58179 = Gan'zulah's Body
@@ -22,7 +23,7 @@
 -- 60334 = Black Bear Brain
 -- 60792 = Pristine Flight Feather (bump from 35% to 50%)
 
-DELETE FROM `creature_loot_template` WHERE `item` IN (884,981,1006,1946,1968,2713,3616,3876,3877,3879,4106,22934,58179,59522,60207,60213,60263,60274,60334,60792);
+DELETE FROM `creature_loot_template` WHERE `item` IN (884,981,1006,1946,1968,2713,3616,3876,3877,3879,3880,4106,22934,58179,59522,60207,60213,60263,60274,60334,60792);
 INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`, `shared`) VALUES
 (1270, 884, -40, 0, 0, 1, 1, 0),
 (948, 884, -40, 0, 0, 1, 1, 0),
@@ -39,6 +40,7 @@ INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `l
 (728, 3876, -100, 0, 0, 1, 1, 0),
 (730, 3877, -100, 0, 0, 1, 1, 0),
 (729, 3879, -100, 0, 0, 1, 1, 0),
+(731, 3880, -100, 0, 0, 1, 1, 0),
 (4260, 4106, -75, 0, 0, 1, 1, 0),
 (1096, 4106, -75, 0, 0, 1, 1, 0),
 (16517, 22934, -100, 0, 0, 1, 1, 0),
